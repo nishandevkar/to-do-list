6727d80bc395bbe0a37bfd53e6aa195606cd8b91
nickthorton <nishandevkarnd@gmail.com>
2024-07-19 00:15:54 +0800

    Adds an input field
    - Logs the input value on submit

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,5 +1,52 @@
+import {
+	FormControl,
+	Input,
+	Heading,
+	Center,
+	FormLabel,
+	Text,
+	Button,
+	FormErrorMessage,
+} from "@chakra-ui/react";
+import { useForm, FieldValues } from "react-hook-form";
+
 const App = () => {
-	return <div>Nishan's to do list</div>;
+	const {
+		handleSubmit,
+		register,
+		formState: { errors },
+	} = useForm();
+
+	const onSubmit = (data: FieldValues) => {
+		console.log(data);
+	};
+	return (
+		<div>
+			<Center marginY={10}>
+				<Heading fontSize="3xl">To Do List</Heading>
+			</Center>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<FormControl>
+					<FormLabel>Add a new item</FormLabel>
+					<Input
+						id="name"
+						placeholder="Start adding..."
+						{...register("name", {
+							required: "This is required",
+							minLength: {
+								value: 4,
+								message: "Minimum length should be 4",
+							},
+						})}
+					/>
+				</FormControl>
+				<Button mt={4} colorScheme="teal" type="submit">
+					Submit
+				</Button>
+			</form>
+			<Text></Text>
+		</div>
+	);
 };
 
 export default App;
